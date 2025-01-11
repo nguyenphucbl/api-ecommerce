a82b91788ab5b919ab7478c34cffb95898ac3dba
nguyenphucbl <phucyoubl@gmail.com>
2025-01-11 16:49:09 +0700

    implement API key authentication and permission checks, add apiKey model and service

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const { findById } = require("../services/apiKey.service");
+
+const HEADER = {
+  API_KEY: "x-api-key",
+  AUTHORIZATION: "authorization",
+};
+
+const apiKey = async (req, res, next) => {
+  try {
+    const key = req.headers[HEADER.API_KEY]?.toString();
+    console.log("🚀 ~ apiKey ~ key:", key);
+
+    if (!key) {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+    // check objKey
+    const objKey = await findById(key);
+    if (!objKey) {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+    req.objKey = objKey;
+    return next();
+  } catch (error) {}
+};
+const permission = (permiss) => {
+  return (req, res, next) => {
+    if (!req.objKey.permissions) {
+      return res.status(403).json({
+        message: "Permission denied",
+      });
+    }
+    console.log(
+      "🚀 ~ permission ~ req.objKey.permissions",
+      req.objKey.permissions
+    );
+    const validPermission = req.objKey.permissions.includes(permiss);
+    if (!validPermission) {
+      return res.status(403).json({
+        message: "Permission denied",
+      });
+    }
+    return next();
+  };
+};
+module.exports = {
+  apiKey,
+  permission,
+};
